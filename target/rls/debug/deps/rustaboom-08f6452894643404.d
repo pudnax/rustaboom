7b1c1e1d33f8989f2c29bfb1c5068bbad63ca776
Alex Komissarov <k.a.komissar@gmail.com>
2019-10-20 18:18:58 +0500

    ?

---
+++ target/rls/debug/deps/rustaboom-08f6452894643404.d
@@ -0,0 +1,6 @@
+/home/komm/Rust/github/rustaboom/target/rls/debug/deps/rustaboom-08f6452894643404.rmeta: src/main.rs src/vec3d.rs
+
+/home/komm/Rust/github/rustaboom/target/rls/debug/deps/rustaboom-08f6452894643404.d: src/main.rs src/vec3d.rs
+
+src/main.rs:
+src/vec3d.rs:
---
+++ target/rls/debug/deps/rustaboom-d30cdb374d21d712.d
@@ -0,0 +1,6 @@
+/home/komm/Rust/github/rustaboom/target/rls/debug/deps/rustaboom-d30cdb374d21d712.rmeta: src/main.rs src/vec3d.rs
+
+/home/komm/Rust/github/rustaboom/target/rls/debug/deps/rustaboom-d30cdb374d21d712.d: src/main.rs src/vec3d.rs
+
+src/main.rs:
+src/vec3d.rs:
